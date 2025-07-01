841780bd7c9dc2e143b9f4f57d70303f60208f5a
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-06-30 21:31:50 -0300

    atualizar

---
+++ ex-01/script.js
@@ -0,0 +1,23 @@
+/* atividade 01 - elaborar um programa que leia um número. Calcule e informe os seus vizinhos, ou seja, o número anterior e posterior.*/
+
+/* const numero = Number(prompt("Digite um número: "));
+const menoscasa = (numero - 1);
+const maiscasa = (numero + 1);
+alert(`antecessor - ${menoscasa} - ${numero} - sucessor ${maiscasa}`); */
+
+/* atividade 02 - elaborar um programa para uma pizzaria, o qual leia o valor total de uma conta e quantos clientes vão pagá-la. Calcule e  infrome o valor a ser pagopor cliente.*/
+
+/* const Conta =  Number(prompt("Valor gasto: "));
+const Nclientes =  Number(prompt("Quantidade de pessoas: "));
+alert(`Valor da conta: ${Conta}\nPessoas: ${Nclientes}\nValor por pessoa: ${Conta / Nclientes}`) */
+
+/* atividade 03 - elaborar um programa para uma loja, o qual leia o preço de um de um produto e informe as opções de pagamentos da loja. Calcule e informe o valor para o pagamento à vista com 10% de desconto e o valor em 3x.*/ 
+
+/* const Prod = Number(prompt("Valor do produto: "));
+alert(`Preço R$: ${Prod}\n À vista R$: ${Prod - (Prod * 0.10)}\n Ou 3x de R$: ${Prod/3}`); */
+
+/* atividade 04 - Elaborar um programa que leia 2 notas de um aluno em uma disciplina. Calcule e informe a média das notas */
+
+const nota1 = Number(prompt("Determine a primeira nota: "));
+const nota2 = Number(prompt("Determine a segunda nota: "));
+alert(`A primeira nota é: ${nota1}\n A segunda nota é: ${nota2}\n A média é: ${(nota1 + nota2) / 2}`);
