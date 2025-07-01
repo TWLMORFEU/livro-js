227be2c27c9bd0fbc9a63b29fe17a2f9c8b44575
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-06-30 21:34:28 -0300

    ATUALIZAR

--- ex-01/script.js
+++ ex-01/script.js
@@ -1,20 +1,20 @@
 /* atividade 01 - elaborar um programa que leia um número. Calcule e informe os seus vizinhos, ou seja, o número anterior e posterior.*/
 
-/* const numero = Number(prompt("Digite um número: "));
+const numero = Number(prompt("Digite um número: "));
 const menoscasa = (numero - 1);
 const maiscasa = (numero + 1);
-alert(`antecessor - ${menoscasa} - ${numero} - sucessor ${maiscasa}`); */
+alert(`antecessor - ${menoscasa} - ${numero} - sucessor ${maiscasa}`);
 
 /* atividade 02 - elaborar um programa para uma pizzaria, o qual leia o valor total de uma conta e quantos clientes vão pagá-la. Calcule e  infrome o valor a ser pagopor cliente.*/
 
-/* const Conta =  Number(prompt("Valor gasto: "));
+const Conta =  Number(prompt("Valor gasto: "));
 const Nclientes =  Number(prompt("Quantidade de pessoas: "));
-alert(`Valor da conta: ${Conta}\nPessoas: ${Nclientes}\nValor por pessoa: ${Conta / Nclientes}`) */
+alert(`Valor da conta: ${Conta}\nPessoas: ${Nclientes}\nValor por pessoa: ${Conta / Nclientes}`) 
 
 /* atividade 03 - elaborar um programa para uma loja, o qual leia o preço de um de um produto e informe as opções de pagamentos da loja. Calcule e informe o valor para o pagamento à vista com 10% de desconto e o valor em 3x.*/ 
 
-/* const Prod = Number(prompt("Valor do produto: "));
-alert(`Preço R$: ${Prod}\n À vista R$: ${Prod - (Prod * 0.10)}\n Ou 3x de R$: ${Prod/3}`); */
+const Prod = Number(prompt("Valor do produto: "));
+alert(`Preço R$: ${Prod}\n À vista R$: ${Prod - (Prod * 0.10)}\n Ou 3x de R$: ${Prod/3}`); 
 
 /* atividade 04 - Elaborar um programa que leia 2 notas de um aluno em uma disciplina. Calcule e informe a média das notas */
 
