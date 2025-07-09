d1301c921060da67fba0f7d959b05afe8a6b2a33
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-09 11:48:57 -0300

    atualizar instruções

--- ex-02/script.js
+++ ex-02/script.js
@@ -8,3 +8,29 @@ frm.addEventListener("submit", (e) => {
     resp.innerText = `Olá ${nome}` // Exibe a resposta do programa
     e.preventDefault() // evita envio do form
 })
+
+
+// informações - programa js que exibe o nome informado pelo usuário no campo de edição
+
+/* 
+
+--- Cria referência ao form e ao elemento h3 ( onde será exibida a resposta ) ---
+
+- const frm = document.querySelector("form")
+- const resp = document.querySelector("h3")
+
+*/
+
+/*
+
+--- Cria um "ouvinte" de evento, acionado quando o botão submit for clicado ---
+
+- frm.addeventListener("submit", (e) => {
+
+- const nome = frm.inNome.value -> obtém o nome digitado
+- resp.innerText = `Olá ${nome}` -> exibe a resposta do programa
+- e.preventdefault() -> evita envio do form
+})
+
+*/
+
