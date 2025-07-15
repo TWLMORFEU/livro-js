b375b578dbafbc2b50f102a0d663afaaa6a6cf9b
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-15 15:44:05 -0300

    atualizar

---
+++ conteúdos/cont-01/script.js
@@ -0,0 +1,26 @@
+/*
+----------PRIORIDADES---------
+
+Parênteses ()
+
+Sempre resolvem primeiro.
+
+Funções do Math
+
+Como Math.pow(), Math.sqrt(), etc.
+
+Executadas com base na ordem do parênteses.
+
+Exponenciação **
+
+Multiplicação *, Divisão /, Resto %
+
+Mesma prioridade. Avaliados da esquerda para a direita.
+
+Adição + e Subtração -
+
+Também com mesma prioridade, e da esquerda para a direita.
+
+
+
+*/
