85d549bafc408a37389d93fe82dccbbca3f6594d
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-15 19:20:24 -0300

    atualizar

--- exercícios/ex-03/script.js
+++ exercícios/ex-03/script.js
@@ -1,5 +1,7 @@
 // Cria referêcia ao form e aos elementos h3 e h4 (resposta)
 
+/* A -> Elaborar um programa para um Cinema, que leia o título e a duração de um filme em minutos. exiba o título do filme do filme e converta a duração para horas e minutos conforme destacado destacado */
+
 const frm = document.querySelector("form");
 const resp1 = document.querySelector("h3");
 const resp2 = document.querySelector("h4");
@@ -15,4 +17,9 @@ frm.addEventListener("submit", (e) => {
     const minutos = duracao % 60
 
     resp1.innerText = titulo
+    resp2.innertext = `${horas} hora(s) e ${minutos} minuto(s)`
+
+    e.preventDefault
 })
+
+/* B -> Elaborar um programa para uma revenda de veículas. O programa deve ler modelo e preço do veículo. Apresentar como resposta o valor da entrada (50%) e o saldo em 12x. A Figura 2.7 ilustra uma execução desse programa.*/
