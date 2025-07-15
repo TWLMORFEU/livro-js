7993359476d2078abc7bf55b7336ad698fa98be7
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-15 20:20:56 -0300

    atualizar

--- exercícios/ex-03/script2.js
+++ exercícios/ex-03/script2.js
@@ -1,2 +1,6 @@
 /* B -> Elaborar um programa para uma revenda de veículas. O programa deve ler modelo e preço do veículo. Apresentar como resposta o valor da entrada (50%) e o saldo em 12x. A Figura 2.7 ilustra uma execução desse programa.*/
 
+// cria refeência ao form e aos elementos de resposta (pelo seu id)
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("")
