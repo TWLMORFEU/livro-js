9b76a73257f995f213a97313a72e63a4a7298763
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-15 20:07:39 -0300

    atualizar

---
+++ exercícios/ex-03/script2.js
@@ -0,0 +1,2 @@
+/* B -> Elaborar um programa para uma revenda de veículas. O programa deve ler modelo e preço do veículo. Apresentar como resposta o valor da entrada (50%) e o saldo em 12x. A Figura 2.7 ilustra uma execução desse programa.*/
+
