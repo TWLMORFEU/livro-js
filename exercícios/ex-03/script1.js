87ae8a35e4a13364176215eb9c435c6915df5ec3
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-15 19:37:20 -0300

    atualizar

--- exercícios/ex-03/script1.js
+++ exercícios/ex-03/script1.js
@@ -21,5 +21,3 @@ frm.addEventListener("submit", (e) => {
 
     e.preventDefault
 })
-
-/* B -> Elaborar um programa para uma revenda de veículas. O programa deve ler modelo e preço do veículo. Apresentar como resposta o valor da entrada (50%) e o saldo em 12x. A Figura 2.7 ilustra uma execução desse programa.*/
