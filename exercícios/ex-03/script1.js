de0554360b58c433e0633109eaaa5649591b6754
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-18 19:58:35 -0300

    x

--- exercícios/ex-03/script1.js
+++ exercícios/ex-03/script1.js
@@ -2,6 +2,7 @@
 
 /* A -> Elaborar um programa para um Cinema, que leia o título e a duração de um filme em minutos. exiba o título do filme do filme e converta a duração para horas e minutos conforme destacado destacado */
 
+/* Cria uma referência ao form e aos elementos h3, h4 (resposta)*/
 const frm = document.querySelector("form");
 const resp1 = document.querySelector("h3");
 const resp2 = document.querySelector("h4");
@@ -10,14 +11,14 @@ const resp2 = document.querySelector("h4");
 // Cria um "ouvinte" de evento, acionado quando o botão submit for clicado 
 
 frm.addEventListener("submit", (e) => {
-    const titulo = frm.ititulo.value 
+    const titulo = frm.ititulo.value // obtém conteúdo dos campos
     const duracao = Number(frm.iduracao.value)
 
-    const horas = Math.floor(duracao / 60)
-    const minutos = duracao % 60
+    const horas = Math.floor(duracao / 60)// arredonda para baixo resultado
+    const minutos = duracao % 60 // obtém o resto da divisão
 
-    resp1.innerText = titulo
+    resp1.innerText = titulo // exibe as respostas
     resp2.innertext = `${horas} hora(s) e ${minutos} minuto(s)`
 
-    e.preventDefault
+    e.preventDefault // envia o envio do form
 })
