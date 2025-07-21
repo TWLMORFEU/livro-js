82ef163a39c963aa82ded7dc001a181f861ce922
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-21 15:28:52 -0300

    atualizar

--- exercícios/ex-03/script3.js
+++ exercícios/ex-03/script3.js
@@ -3,6 +3,16 @@ C) Elaborar um programa para um restaurante que leia o preço por kg e o consumo
 */
 
 // cria referência ao form e ao elemento h3 ( onde será exibida a resposta)
-const frm = document.querySelector("form")
-const resp = document.querySelector("h3")
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
 
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e) => {
+    const quilo = Number(frm.iquilo.value); // obtêm o conteúdo dos campos
+    const Consumo = Number(frm.icons.value)
+
+    const valor = (quilo / 1000) * Consumo // calcula valor a ser pago
+    resp.innerText = `Valor a pagar R$: ${valor.toFixed(2)}` // Exibe resposta
+
+    e.preventDefault // evita o envio do formulario
+})
