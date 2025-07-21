d8de8c1cc95b2ca3b84789b3c5058762fe1a3369
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-21 15:16:05 -0300

    atualizar

---
+++ exercícios/ex-03/script3.js
@@ -0,0 +1,8 @@
+/*
+C) Elaborar um programa para um restaurante que leia o preço por kg e o consumo (em gramas) de um cliente. Exiba o valor a ser pago, conforme ilu
+*/
+
+// cria referência ao form e ao elemento h3 ( onde será exibida a resposta)
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
