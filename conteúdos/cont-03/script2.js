d852761ace016aa96c60150b437d9ac0644b65e8
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-25 23:37:15 -0300

    a

--- conteúdos/cont-03/script2.js
+++ conteúdos/cont-03/script2.js
@@ -5,6 +5,9 @@ const veiculo = prompt("veículo: ") // l~e os dados de entrada
 const preco = Number(prompt('Preço R$: '))
 const entrada = preco * 0.50 //calcula valor de entrada
 
-
-
+const entrada = preco * 0.50
+const parcela = (preco * 0.50) / 12 
+console.log(`Promoção: ${veiculo}`)
+console.log(`Entrada de R$: ${entrada.toFixed(2)}`)
+console.log(`+12x de R$ ${parcela.toFixed(2)}`)
 
