c2c4178fc870cb24cad522a90f3fa53e68f487ad
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-26 14:39:36 -0300

    atualizar

--- conteúdos/cont-03/script2.js
+++ conteúdos/cont-03/script2.js
@@ -8,5 +8,5 @@ const entrada = preco * 0.50 //calcula valor de entrada
 const parcela = (preco * 0.50) / 12 // ... e das p
 console.log(`Promoção: ${veiculo}`) // exibe 
 console.log(`Entrada de R$: ${entrada.toFixed(2)}`) 
-console.log(`+12x de R$ ${parcela.toFixed(2)}`
+console.log(`+12x de R$ ${parcela.toFixed(2)}`)
 
