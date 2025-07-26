b74ff8d823e302eeda8ff861d1e5552173e64382
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-07-26 14:40:40 -0300

    finalizando

--- conteúdos/cont-03/script2.js
+++ conteúdos/cont-03/script2.js
@@ -5,8 +5,8 @@ const veiculo = prompt("veículo: ") // l~e os dados de entrada
 const preco = Number(prompt('Preço R$: '))
 const entrada = preco * 0.50 //calcula valor de entrada
 
-const parcela = (preco * 0.50) / 12 // ... e das p
-console.log(`Promoção: ${veiculo}`) // exibe 
+const parcela = (preco * 0.50) / 12 // ... e das parcelas
+console.log(`Promoção: ${veiculo}`) // exibe as respostas
 console.log(`Entrada de R$: ${entrada.toFixed(2)}`) 
 console.log(`+12x de R$ ${parcela.toFixed(2)}`)
 
