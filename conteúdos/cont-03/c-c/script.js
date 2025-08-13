f7341844cf1e6bfeb6a145b56cacdf2ebdd44d30
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-13 14:42:23 -0300

    atualizar correção de exercícios e inclusão de anotações

--- conteúdos/cont-03/c-c/script.js
+++ conteúdos/cont-03/c-c/script.js
@@ -1,5 +1,5 @@
 /*
-C) Em um determinado momento do dia, apenas notas de 10,50 e 100 estão disponiveis em um terminal de caixa eletrônico. Elaborar um programa que leia um valor de saque de um cliente, verifique sua validade (ou seja, se pode se pago com as notas disponíveis) e informe o número minímo de notas de 100, 50 e 10 necessárias para pagar esse saque.
+C) Em um determinado momento do dia, apenas notas de 10,50 e 100 estão disponiveis em um terminal de caixa eletrônico. Elaborar um programa que leia um valor de saque de um cliente, verifique sua validade ou seja, se pode se pago com as notas disponíveis e informe o número minímo de notas de 100, 50 e 10 necessárias para pagar esse saque.
 */
 
 const frm = document.querySelector("form") // obtém elementos da pagina
@@ -12,7 +12,7 @@ frm.addEventListener("submit", (e) => {              // obtém elementos da pág
     const saque = Number(frm.inSaque.value)          // obtém o valor do saque 
     if (saque % 10 != 0) {                           // se saque não é múltiplo de 10
        alert("valor inválido para notas disponíveis (R$ 10, 50, 100)")
-       frm.insaqu.focus()
+       frm.inSaque .focus()
        return
     }
 
@@ -31,3 +31,5 @@ frm.addEventListener("submit", (e) => {              // obtém elementos da pág
         resp3.innerText = `Notas de R$ 10: ${notaDez}`
     }
 })
+/* A estrutura segue a seguinte ordem, primeiro dentro do evento de submit, ela ira verificar se o numero é divisel por 10, afinal todas as notas (10, 50, 100) são multiplas de dez, e dentre isso, se caso não for é utilizado um return para um reconhecimento de numero inválido.
+Em seguinte, ha uma sequencia de verificações, onde verifica quantas vezes pode se dividir pela maior nota ate a menor, e durante isso apos dividir pela maior, o resto vai para uma variavel que sera divisivel por outra e mais outra ate que esteja completa */   
