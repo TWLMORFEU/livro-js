311d5e7fe3dc79dc5dc4c4bd381197196db25ce3
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-14 16:30:54 -0300

    segundo exercicio agora concluido de verdade

--- exercícios/ex-09/ex-b/script.js
+++ exercícios/ex-09/ex-b/script.js
@@ -1,5 +1,26 @@
 /*
-B) Uma farmácia necessita de um programa que leia o total de uma compra. Exiba como a resposta o número máximo de vezes que o clientee pode parcelar essa compra e o valor de cada parcela. Considere as seguintes condições: A) cada parcela deve ser de, no minímo, R$ 20,00; B) o número máximo de parcelas permitido é 6
+B) elaborar um programa que leeia a velocidade permitida em uma estrada e a velocidade de um condutor. Se a velocidade for inferior ou igual à permitida, exiba "Sem Multa". Se a velocidade for de até 20% maior que a permitida, exiba "Multa Leve". E se, a velocidade for superior a 20% da velocidade permitida, exiba "Multa Grave"
 */
 
 
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const perm = Number(frm.ivel.value);
+    const cond = Number(frm.ivec.value);
+
+    const medi = (perm / 100) * 20 + perm;
+    
+    let re
+    if (cond <= perm) {
+       re = "Sem multa"
+    } else if (cond === medi) {
+        re = "Multa Leve"
+    } else {
+        re = "Multa Grave"
+    }
+
+    resp.innerText = `Situação: $`;
+})
