7a23253282f2938b82d857663b2f9ac85c89bad8
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-14 14:24:52 -0300

    inicializando segundo exercício

---
+++ exercícios/ex-09/ex-b/script.js
@@ -0,0 +1,5 @@
+/*
+B) Uma farmácia necessita de um programa que leia o total de uma compra. Exiba como a resposta o número máximo de vezes que o clientee pode parcelar essa compra e o valor de cada parcela. Considere as seguintes condições: A) cada parcela deve ser de, no minímo, R$ 20,00; B) o número máximo de parcelas permitido é 6
+*/
+
+
