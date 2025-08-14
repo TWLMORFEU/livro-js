a0dbad45d2a9c09c67d0ed98a610f9486c72cf0a
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-14 17:20:23 -0300

    pausa

---
+++ exercícios/ex-09/ex-c/script.js
@@ -0,0 +1,5 @@
+/*
+C) Elaborar um programa para simular um parquímetro, o qual leia o valor de moedas depositado em um terminal de estacionamento rotativo. O programa deve  informar o tempo da permanência do veículo no local e o troco (se existir)
+*/
+
+
