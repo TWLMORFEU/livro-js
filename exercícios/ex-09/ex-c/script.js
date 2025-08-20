0644ae8606a4916c25ee8567432f496110ef2697
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-20 17:15:18 -0300

    atualização final de exercício

--- exercícios/ex-09/ex-c/script.js
+++ exercícios/ex-09/ex-c/script.js
@@ -3,15 +3,31 @@ C) Elaborar um programa para simular um parquímetro, o qual leia o valor de moe
 */
 
 
-const valor = document.querySelector("form")
-let respOne = document.querySelector("h3")
-let respTwo = document.querySelector("h3")
+const frm = document.querySelector("form")
+let respOne = document.querySelector("#respOne")
+let respTwo = document.querySelector("#respTwo")
 
 let tempo = [30, 60, 120]
 let preco = [1.00, 1.75, 3.00]
 
-valor.addEventListener("submit",  (e) => {
-    if (valor > preco[1] && valor < preco[2]) {
-       res
+frm.addEventListener("submit",  (e) => {
+     e.preventDefault();
+    let valor = Number(frm.ipar.value)
+   
+    if (valor >= preco[0] && valor < preco[1]) {
+       respOne.textContent = `Tempo: ${tempo[0]} minutos`
+       respTwo.textContent = `Troco: ${(valor - preco[0]).toFixed(2)}`
+
+    } else if (valor >= preco[1] && valor < preco[2]) {
+       respOne.textContent = `Tempo: ${tempo[1]} minutos` 
+       respTwo.textContent = `Troco: ${(valor - preco[1]).toFixed(2)}`
+
+    } else if (valor >= preco[2]) {
+        respOne.textContent =` Tempo: ${tempo[2]}`
+        respTwo.textContent = `Troco: ${(valor - preco[3].toFixed(2))}`
+
+    } else {
+        respOne.textContent = "Valor Insuficiente"
+        respTwo.textContent = ''
     }
 })
