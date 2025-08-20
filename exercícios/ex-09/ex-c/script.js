cd5352c826adc9f8cf96dc0829e6b36f9ef0c0da
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-20 15:54:43 -0300

    inicializando estrutura js

--- exercícios/ex-09/ex-c/script.js
+++ exercícios/ex-09/ex-c/script.js
@@ -3,3 +3,15 @@ C) Elaborar um programa para simular um parquímetro, o qual leia o valor de moe
 */
 
 
+const valor = document.querySelector("form")
+let respOne = document.querySelector("h3")
+let respTwo = document.querySelector("h3")
+
+let tempo = [30, 60, 120]
+let preco = [1.00, 1.75, 3.00]
+
+valor.addEventListener("submit",  (e) => {
+    if (valor > preco[1] && valor < preco[2]) {
+       res
+    }
+})
