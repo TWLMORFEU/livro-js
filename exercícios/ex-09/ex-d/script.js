09a64130b3c11190cf00c6cdbf7822f9d4bb37b6
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-21 07:23:26 -0300

    atualizar código

---
+++ exercícios/ex-09/ex-d/script.js
@@ -0,0 +1,5 @@
+/*
+D) Elaborar um programa que leia três lados e verifique se eles podem ou nâo pode ser maior que a soma dos outros dois. Caso possam formar um triângulo, exiba também qual o tipo do triângulo: Equilátero (3 lados iguais), Isóscele (2 lados iguais), escaleno (3 lados diferentes)
+*/
+
+
