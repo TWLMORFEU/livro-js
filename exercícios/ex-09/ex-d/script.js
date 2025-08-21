5d51ae6da9fba8443838a2d2b70fee49344e0687
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-08-21 08:39:31 -0300

    finalizando exercicio 4

--- exercícios/ex-09/ex-d/script.js
+++ exercícios/ex-09/ex-d/script.js
@@ -3,3 +3,30 @@ D) Elaborar um programa que leia três lados e verifique se eles podem ou nâo p
 */
 
 
+const frm = document.querySelector("form");
+const resp = document.querySelector('#h3ti');
+let msg;
+
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let ladoa = (frm.iladoA.value);
+    let ladob = (frm.iladoB.value);
+    let ladoc = (frm.iladoC.value);
+
+    const [A, B, C] = [ladoa, ladob, ladoc]
+   
+    if (A >= B + C || B >= A + C || C >= A + B) {
+        msg = "Os lados não determinam um triângulo"
+    } else if (A === B && B === C) {
+       msg = 'Equilátero'
+    } else if (A === B || B === C || C === A) {
+        msg = 'Isósceles' 
+    } else {
+        msg = 'Escaleno'
+    }
+
+    resp.innerText = `Triângulo:  ${msg}`;
+})
+
