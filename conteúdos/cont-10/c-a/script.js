bfbcfc9361def6f4ca42449224dfb3d9da118a56
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2025-11-03 14:54:43 -0300

    atualizar base de comando cd em node

--- conteúdos/cont-10/c-a/script.js
+++ conteúdos/cont-10/c-a/script.js
@@ -3,113 +3,25 @@ A) A Copa do Mundo ocorre de 4 em 4 anos, desde 1930, exceto nos anos de 1942 e 
 */
  
 
-const 
+const  prompt = require("prompt-sync")()
+console.log("Programa Anos de copa do mundo. Digite 0 para sair ")
+console.log('--------------------------------------------------')
 
+do { 
+    const ano = Number(prompt("Ano: ")) 
+    if (ano == 0) {
+        break // sai da repetição
+    } else if (ano === 1942 || ano === 1946) {
+        console.log(`Não houve Copa em ${ano} (Segunda Guerra Mundial)`)
+    } else if (ano >= 1930 && ano % 4 === 2) {
+        console.log(`Sim! ${ano} é ano de Copa do Mundo!`)
+    } else {
+        console.log(`Não...${ano} não é um ano de Copa do Mundo.`)
+    }
+}while(true)
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* Foi usada uma estrategia legal, pega o ano de inicio, e ja que e em 4 em 4 ele divide por 4 e todos os divisores a partir dali serao os mesmos, no caso o dois */
