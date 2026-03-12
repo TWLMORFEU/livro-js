19ea3c23ce2dad9c785480c0020221a3c6951a2a
pr0t0n <pr0t0n.p1ankt0n.34521@gmail.com>
2026-03-12 14:54:32 -0300

    bobeira

--- conteúdos/cont-10/c-c/script.js
+++ conteúdos/cont-10/c-c/script.js
@@ -1,3 +1,4 @@
 /*
 c) Elaborar um programa para uma loja que leia o valor de uma conta e o número de vezes que um cliente deseja parcelar esse valor (em bolets ou carnê). Para facilitar o troco, o logista deseja que as parcelas iniciais não tenham centavos, ou seja, centavos apenas na última parcela. Informe como resposta o valor de cada parcela considerando esta situação.
 */
+8i
